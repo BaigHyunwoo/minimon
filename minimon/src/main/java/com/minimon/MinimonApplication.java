c47725e1327acd851e35d7c0160c47eeb9b694bf
Baig Hyunwoo <qorto12@naver.com>
2019-07-29 22:12:27 +0900

    07/29 22:12 Mini monitoring Project Create - H

---
+++ minimon/src/main/java/com/minimon/MinimonApplication.java
@@ -0,0 +1,13 @@
+package com.minimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimonApplication.class, args);
+	}
+
+}
