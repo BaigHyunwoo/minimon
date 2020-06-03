f5c392b5b50f737cae7f170484ef93e6c6b1753b
ethan <lom1992@daum.net>
2020-06-03 10:52:28 +0900

    Add: smsService for sending errorLog

--- minimon/src/main/java/com/minimon/MinimonApplication.java
+++ minimon/src/main/java/com/minimon/MinimonApplication.java
@@ -20,6 +20,7 @@ public class MinimonApplication {
 
 	public static void setDriverPath(String driverPath) {
 		MinimonApplication.driverPath = driverPath+File.separator+"chromedriver.exe";
+
 	}
 
 
---
+++ minimon/src/main/java/com/minimon/service/smsService.java
@@ -0,0 +1,34 @@
+package com.minimon.service;
+
+import com.minimon.entity.TblMonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class smsService {
+
+    private Logger logger = LoggerFactory.getLogger(smsService.class);
+
+    public void sendSimpleMessage(String to, String subject, TblMonResult tblMonResult) throws MessagingException {
+        String text = new StringBuffer()
+                .append("<h1>모니터링 결과입니다.</h1>")
+                .append("<p> 타이틀 : "+tblMonResult.getTitle()+"</p>")
+                .append("<p> 종류 : "+tblMonResult.getType()+"</p>")
+                .append("<p> SEQ : "+tblMonResult.getMon_seq()+"</p>")
+                .append("<p> 일시 : "+tblMonResult.getRegDate()+"</p>")
+                .append("<p> 결과 : "+tblMonResult.getResult()+"</p>")
+                .append("<p> 걸린 시간 : "+tblMonResult.getLoadTime()+"Ms</p>")
+                .append("<p> 응답 : "+tblMonResult.getResponse()+"</p>")
+                .toString();
+        //emailSender.send(message);
+
+        logger.info("SEND MAIL : "+to+"  Body : "+text);
+    }
+}
