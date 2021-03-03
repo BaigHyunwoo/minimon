9452a05e8074d8aa0d71bb01142fd03d2bcdfb9e
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-03 23:13:35 +0900

    querydsl을 repository에서 함께 사용하기 위한 QueryDslRepositorySupportWrapper abstract class 생성 및 querydsl 적용

--- minimon/src/main/java/com/minimon/repository/MonUrlRepository.java
+++ minimon/src/main/java/com/minimon/repository/MonUrlRepository.java
@@ -1,13 +1,36 @@
 package com.minimon.repository;
 
 import com.minimon.entity.MonUrl;
+import com.minimon.entity.QMonUrl;
+import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface MonUrlRepository extends JpaRepository<MonUrl, Integer> {
-	List<MonUrl> findByMonitoringUseYn(String monitoringUseYn);
+    List<MonUrl> findByMonitoringUseYn(String monitoringUseYn);
+}
+
+interface MonUrlSupport {
+    List<MonUrl> findByText(String text);
+
+    @Transactional
+    class MonUrlSupportImpl extends QueryDslRepositorySupportWrapper implements MonUrlSupport {
+        QMonUrl qMonUrl = QMonUrl.monUrl;
+
+        public MonUrlSupportImpl() {
+            super(MonUrl.class);
+        }
+
+        @Override
+        public List<MonUrl> findByText(String text) {
+            JPQLQuery query = from(qMonUrl)
+                    .where(qMonUrl.title.like(text))
+                    .where(qMonUrl.url.like(text));
+            return query.fetch();
+        }
+    }
 }
