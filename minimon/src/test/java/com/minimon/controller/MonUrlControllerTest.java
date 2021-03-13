22963db0c73647987cb42f62913e23e6c8d9500e
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-13 23:47:41 +0900

    test: Url 모니터링 등록 테스트 추가

--- minimon/src/test/java/com/minimon/controller/MonUrlControllerTest.java
+++ minimon/src/test/java/com/minimon/controller/MonUrlControllerTest.java
@@ -1,22 +1,21 @@
 package com.minimon.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.minimon.service.MonUrlService;
+import com.minimon.entity.MonUrl;
 import com.minimon.vo.MonUrlCheckVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.StopWatch;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -29,6 +28,25 @@ public class MonUrlControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    public void create() throws Exception {
+        String content = objectMapper.writeValueAsString(MonUrl.builder()
+                .url("https://www.naver.com")
+                .title("네이버")
+                .errorLoadTime(3000)
+                .timeout(5)
+                .loadTime(2000)
+                .build());
+
+        mockMvc.perform(post("/monUrl")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.meta.code", is(200)))
+                .andDo(print());
+    }
 
     @Test
     public void check() throws Exception {
