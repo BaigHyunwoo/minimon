5ad35544069c915a2680578589cce9776ab9cf36
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-15 22:51:56 +0900

    feat: API 모니터링 검사 VO 추가

---
+++ minimon/src/main/java/com/minimon/vo/MonApiCheckVO.java
@@ -0,0 +1,27 @@
+package com.minimon.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@ApiModel(value = "API 검사 VO")
+public class MonApiCheckVO {
+
+    @ApiModelProperty(value = "검사 Http method")
+    private String method;
+
+    @ApiModelProperty(value = "검사 경로")
+    private String url;
+
+    @ApiModelProperty(value = "검사 데이터")
+    private String data;
+
+    @Builder
+    public MonApiCheckVO(String method, String url, String data) {
+        this.method = method;
+        this.url = url;
+        this.data = data;
+    }
+}
