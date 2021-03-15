a95c123afec13d8e7c8ad002bbcd4733201c2a78
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-15 23:11:19 +0900

    test: API 모니터링 검사 테스트 추가

--- minimon/src/test/java/com/minimon/controller/MonUrlControllerTest.java
+++ minimon/src/test/java/com/minimon/controller/MonUrlControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -46,6 +46,18 @@ public class MonUrlControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.meta.code", is(ResponseEnum.SUCCESS.getCode())))
+                .andDo(print());
+    }
+
+    @Test
+    public void check() throws Exception {
+        String content = objectMapper.writeValueAsString(MonUrlCheckVO.builder().url("https://www.naver.com").timeout(5).build());
+
+        mockMvc.perform(post("/monUrl/check")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andDo(print());
     }
 }
