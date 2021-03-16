6c5c9ede9b8e14abdfe694167786d2a5185f3bd2
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-16 22:05:06 +0900

    test: ACT 모니터링 test 추가

---
+++ minimon/src/test/java/com/minimon/controller/MonActControllerTest.java
@@ -0,0 +1,32 @@
+package com.minimon.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.minimon.entity.MonUrl;
+import com.minimon.enums.ResponseEnum;
+import com.minimon.vo.MonUrlCheckVO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class MonActControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
