657bd5ae49445544fcb358ae4568f6c1d18df021
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-17 22:58:56 +0900

    test: URL 모니터링 조회 test 추가

--- minimon/src/test/java/com/minimon/controller/MonUrlControllerTest.java
+++ minimon/src/test/java/com/minimon/controller/MonUrlControllerTest.java
@@ -3,6 +3,7 @@ package com.minimon.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minimon.entity.MonUrl;
 import com.minimon.enums.ResponseEnum;
+import com.minimon.service.MonUrlService;
 import com.minimon.vo.MonUrlCheckVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +15,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,18 +32,48 @@ public class MonUrlControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    public void create() throws Exception {
-        String content = objectMapper.writeValueAsString(MonUrl.builder()
+    @Autowired
+    private MonUrlService monUrlService;
+
+
+    private MonUrl getDefaultMonUrl(){
+        return MonUrl.builder()
                 .url("https://www.naver.com")
                 .title("네이버")
                 .errorLoadTime(3000)
                 .timeout(5)
                 .loadTime(2000)
-                .build());
+                .build();
+    }
 
+    @Test
+    public void getUrl() throws Exception {
+        MonUrl monUrl = monUrlService.save(getDefaultMonUrl());
+
+        mockMvc.perform(get("/monUrl/"+monUrl.getSeq())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.meta.code", is(ResponseEnum.SUCCESS.getCode())))
+                .andExpect(jsonPath("$.data.url", is(monUrl.getUrl())))
+                .andDo(print());
+    }
+
+    @Test
+    public void getUrlList() throws Exception {
+        monUrlService.save(getDefaultMonUrl());
+
+        mockMvc.perform(get("/monUrl")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.meta.code", is(ResponseEnum.SUCCESS.getCode())))
+                .andExpect(jsonPath("$.data.totalElements", not(0)))
+                .andDo(print());
+    }
+
+    @Test
+    public void create() throws Exception {
         mockMvc.perform(post("/monUrl")
-                .content(content)
+                .content(objectMapper.writeValueAsString(getDefaultMonUrl()))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
