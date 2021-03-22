5fe6e2e350ffd52aeaaf73800df44c250bd22fc8
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-22 22:33:04 +0900

    feat: webConfig 수정 index.html 첫 접근화면으로 변경

--- minimon/src/main/java/com/minimon/config/WebConfig.java
+++ minimon/src/main/java/com/minimon/config/WebConfig.java
@@ -2,16 +2,20 @@ package com.minimon.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
-	@Bean
-	public MappingJackson2JsonView jsonView() {
-		return new MappingJackson2JsonView();
-	}
-	
-	
+
+    @Bean
+    public MappingJackson2JsonView jsonView() {
+        return new MappingJackson2JsonView();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/view/main/index");
+    }
 }
