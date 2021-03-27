74ef7384578afdfac97a22de076ccbdbb432ea05
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-03-27 19:46:47 +0900

    test: 스케줄러 유닛 테스트 추가

---
+++ minimon/src/test/java/com/minimon/scheduler/CustomSchedulerTest.java
@@ -0,0 +1,81 @@
+package com.minimon.scheduler;
+
+import com.minimon.enums.SchedulerTypeEnum;
+import com.minimon.vo.RunningSchedulerVO;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class CustomSchedulerTest {
+
+    @Autowired
+    private CustomScheduler customScheduler;
+
+    @Test
+    void getRunningScheduler() {
+        RunningSchedulerVO runningSchedulerVO = customScheduler.getRunningScheduler();
+        assertNotEquals(0, runningSchedulerVO.getRunningTaskCount());
+        assertNotEquals(0, runningSchedulerVO.getRunningTaskCount());
+    }
+
+    @Test
+    void stopAllTasks() {
+        int totalTaskSize = SchedulerTypeEnum.values().length;
+
+        totalTaskSize -= totalTaskSize;
+        customScheduler.stopAllTasks();
+        assertEquals(totalTaskSize, customScheduler.getRunningScheduler().getRunningTaskCount());
+    }
+
+    @Test
+    void runAllTasks() {
+        int totalTaskSize = SchedulerTypeEnum.values().length;
+
+        totalTaskSize -= totalTaskSize;
+        customScheduler.stopAllTasks();
+        assertEquals(totalTaskSize, customScheduler.getRunningScheduler().getRunningTaskCount());
+
+        totalTaskSize += SchedulerTypeEnum.values().length;
+        customScheduler.runAllTasks();
+        assertEquals(totalTaskSize, customScheduler.getRunningScheduler().getRunningTaskCount());
+    }
+
+    @Test
+    void run() {
+        int totalTaskSize = SchedulerTypeEnum.values().length;
+
+        totalTaskSize -= totalTaskSize;
+        customScheduler.stopAllTasks();
+        assertEquals(totalTaskSize, customScheduler.getRunningScheduler().getRunningTaskCount());
+
+        totalTaskSize++;
+        assertEquals(true, customScheduler.run(SchedulerTypeEnum.URL_MONITORING));
+        assertEquals(totalTaskSize, customScheduler.getRunningScheduler().getRunningTaskCount());
+    }
+
+    @Test
+    void stop() {
+        int totalTaskSize = SchedulerTypeEnum.values().length;
+
+        totalTaskSize--;
+        assertEquals(true, customScheduler.stop(SchedulerTypeEnum.URL_MONITORING));
+        assertEquals(totalTaskSize, customScheduler.getRunningScheduler().getRunningTaskCount());
+
+        totalTaskSize--;
+        assertEquals(true, customScheduler.stop(SchedulerTypeEnum.API_MONITORING));
+        assertEquals(totalTaskSize, customScheduler.getRunningScheduler().getRunningTaskCount());
+    }
+
+    @Test
+    void execute() {
+        assertEquals(true, customScheduler.execute(SchedulerTypeEnum.API_MONITORING));
+    }
+}
