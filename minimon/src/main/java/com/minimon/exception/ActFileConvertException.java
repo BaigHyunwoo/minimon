2e0a44e806bac54a6b0c6dee849931e57eadc38a
BaigHyunwoo <ethan.baig.hw@gmail.com>
2021-04-11 23:16:13 +0900

    feat: ACT 모니터링 파일 변환 예외 생성

---
+++ minimon/src/main/java/com/minimon/exception/ActFileConvertException.java
@@ -0,0 +1,11 @@
+package com.minimon.exception;
+
+public class ActFileConvertException extends RuntimeException {
+    public ActFileConvertException(Throwable t) {
+        super(t);
+    }
+
+    public ActFileConvertException() {
+        super();
+    }
+}
